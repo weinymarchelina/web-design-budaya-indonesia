53bc481e4fdfba4e8348b354e516991f5ddf4bdd
weinymarchelina <weinymarchelina31@gmail.com>
2021-09-03 23:33:13 +0800

    add pasue features

--- script.js
+++ script.js
@@ -18,3 +18,61 @@ const showSlides = () => {
 };
 
 showSlides();
+
+const slideTrack = document.querySelector(".slide-track");
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach((slide) => {
+  slide.addEventListener("click", () => {
+    const text = slide.querySelector("p");
+
+    if (slideTrack.style.animationPlayState !== "paused") {
+      slideTrack.style.animationPlayState = "paused";
+      text.style.opacity = "1";
+    } else {
+      slideTrack.style.animationPlayState = "running";
+      text.style.opacity = "0";
+    }
+  });
+});
+
+const galleryRows = document.querySelectorAll(".gallery-row");
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    const text = box.querySelector("p");
+
+    text.style.opacity = "1";
+    galleryRows.forEach((row) => {
+      row.style.animationPlayState = "paused";
+    });
+
+    setTimeout(() => {
+      text.style.opacity = "0";
+      galleryRows.forEach((row) => {
+        row.style.animationPlayState = "running";
+      });
+    }, 2500);
+
+    // galleryRows.forEach((row) => {
+    //   row.addEventListener("click", () => {
+    //     if (row.style.animationPlayState !== "paused") {
+    //       text.style.opacity = "1";
+    //       console.log("paused");
+
+    //       galleryRows.forEach((row) => {
+    //         row.style.animationPlayState = "paused";
+    //       });
+    //     } else {
+    //       text.style.opacity = "0";
+    //       console.log("running");
+
+    //       galleryRows.forEach((row) => {
+    //         row.style.animationPlayState = "running";
+    //       });
+    //     }
+    //   });
+    // });
+  });
+});
